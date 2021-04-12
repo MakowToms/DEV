4c53ef915f75e1056f941bfe035e670ad2876f7d
boguckiw <boguckiw@student.mini.pw.edu.pl>
2021-04-12 20:02:42 +0200

    Improvements

--- src/PD3/pd3.py
+++ src/PD3/pd3.py
@@ -12,6 +12,7 @@ def gap_function(x, y):
         return -2
     return -(2 + y/4.0 + log(y)/2.0)
 
+# read sequences
 dir = "data/PD3_data"
 filenames = os.listdir(dir)
 filepaths = [os.path.join(dir, filename) for filename in filenames]
@@ -19,8 +20,12 @@ sequences = []
 for f in filepaths:
     sequences.append(SeqIO.read(f, "fasta").seq)
 
-
+# alignment example
 alignment = pairwise2.align.globalmc(sequences[0], sequences[1], 5, -4, gap_function, gap_function)
 
+# get score
+print(alignment[0].score)
+
+# print info
 for a in alignment:
     print(format_alignment(*a))
